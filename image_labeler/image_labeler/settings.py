c39a6f565799771c43748dd9878655aa0a272cc4
clip.art.monster <admin@clip.art.monster>
2024-10-15 09:11:45 -0500

    added gunicorn

--- image_labeler/image_labeler/settings.py
+++ image_labeler/image_labeler/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-print('-------------------')
-print(BASE_DIR)
+
 
 
 # Default primary key field type
