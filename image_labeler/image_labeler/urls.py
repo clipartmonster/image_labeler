4c9a2b80be4038ca5c53db3f304b215f92b0bd7e
noahschwarzkopf <noahschwarzkopf98@gmail.com>
2026-01-21 12:30:58 -0500

    Fix 500 error and add root redirect

--- image_labeler/image_labeler/urls.py
+++ image_labeler/image_labeler/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from .views import get_config
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("label_images/", include('label_images.urls')),
-    path('get_config/', get_config, name = 'get_config')
+    path('get_config/', get_config, name = 'get_config'),
+    path('', lambda request: redirect('label_images/front_page/', permanent=False)),
 ]
